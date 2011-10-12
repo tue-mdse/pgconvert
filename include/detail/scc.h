a0472cd8f95ca7c793c99e8fb476424794964bf8
scranen <scranen@f3a488e2-441e-0410-a9fe-873d317e53d2>
2011-10-12 10:00:34 +0000

    Keep node 0 at position 0 in SCC reduction.

--- include/detail/scc.h
+++ include/detail/scc.h
@@ -2,11 +2,21 @@
 #define __GRAPH_IMPL_SCC_H
 
 #include "vertex.h"
+#ifdef __GNU_LIBRARY__
+#include <ext/slist>
+#else
 #include <list>
+#endif
 #include <vector>
 
 namespace graph {
 namespace impl {
+
+#ifdef __GNU_LIBRARY__
+  typedef __gnu_cxx::slist<size_t> stack_t; ///< List of vertices (used when VertexSet is too expensive).
+#else
+  typedef std::list<size_t> stack_t; ///< List of vertices (used when VertexSet is too expensive).
+#endif
 
 /*
  * Iterative implementation of Tarjan's SCC algorithm.
@@ -17,11 +27,10 @@ namespace impl {
 template <typename Vertex>
 void tarjan_iterative(std::vector<Vertex>& vertices, std::vector<VertexIndex>& scc)
 {
-	size_t debug =0;
 	size_t unused = 1, lastscc = 1;
 	std::vector<size_t> low;
-	std::list<size_t> stack;
-	std::list<size_t> sccstack;
+	stack_t stack;
+	stack_t sccstack;
 	low.resize(vertices.size(), 0);
 	for (size_t i = 0; i < vertices.size(); ++i)
 	{
@@ -36,8 +45,8 @@ void tarjan_iterative(std::vector<Vertex>& vertices, std::vector<VertexIndex>& s
 			{
 				scc[vi] = unused;
 				low[vi] = unused++;
-				sccstack.push_back(vi);
-				for (graph::VertexSet::iterator w = v.out.begin(); w != v.out.end(); ++w)
+				sccstack.push_front(vi);
+				for (graph::VertexSet::const_iterator w = v.out.begin(); w != v.out.end(); ++w)
 				{
 					if ((low[*w] == 0) and (scc[*w] == 0) and (vertices[*w].label == v.label))
 						stack.push_front(*w);
@@ -45,7 +54,7 @@ void tarjan_iterative(std::vector<Vertex>& vertices, std::vector<VertexIndex>& s
 			}
 			else
 			{
-				for (graph::VertexSet::iterator w = v.out.begin(); w != v.out.end(); ++w)
+				for (graph::VertexSet::const_iterator w = v.out.begin(); w != v.out.end(); ++w)
 				{
 					if ((low[*w] != 0) and (vertices[*w].label == v.label))
 						low[vi] = low[vi] < low[*w] ? low[vi] : low[*w];
@@ -55,11 +64,10 @@ void tarjan_iterative(std::vector<Vertex>& vertices, std::vector<VertexIndex>& s
 					size_t tos, scc_id = lastscc++;
 					do
 					{
-						++debug;
-						tos = sccstack.back();
+						tos = sccstack.front();
 						low[tos] = 0;
 						scc[tos] = scc_id;
-						sccstack.pop_back();
+						sccstack.pop_front();
 					}
 					while (tos != vi);
 				}
@@ -73,9 +81,20 @@ template <typename Vertex>
 void collapse(std::vector<Vertex>& vertices, std::vector<VertexIndex>& sccs)
 {
 	graph::VertexSet temp;
-	// Replace incoming/outgoing node indices by corresponding scc indices.
+
+	// Replace incoming/outgoing node indices by corresponding scc indices. Also
+	// make sure that vertex 0 is still vertex 0 after collapsing.
+	VertexIndex scc0 = sccs[0];
 	for (size_t i = 0; i < vertices.size(); ++i)
-		--sccs[i];
+	{
+    if (sccs[i] == scc0)
+      sccs[i] = 0;
+    else if (sccs[i] == 0)
+      sccs[i] = scc0 - 1;
+    else
+      --sccs[i];
+	}
+
 	for (size_t i = 0; i < vertices.size(); ++i)
 	{
 		temp.clear();
